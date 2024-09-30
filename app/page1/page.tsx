1b4f0d0330ef49b8af1a09a9cf0392d5a7e3d6e7
dheeraj <dheerajrvithalkar@gmail.com>
2024-09-30 11:44:14 +0530

    prod issues fix

--- app/page1/page.tsx
+++ app/page1/page.tsx
@@ -3,12 +3,16 @@ import { useSession } from "next-auth/react";
 
 export default function Page1() {
   const { data: session, update } = useSession();
+
+  const handleUpdate=()=>{
+    if(session?.user && update){
+      update({...session.user, name: "Shivshakti"})
+    }
+  }
   return (
     <>
       <button
-        onClick={() => {
-          session?.user && update({ ...session.user, name: "Shivshakti" });
-        }}>
+        onClick={handleUpdate}>
         Update session
       </button>
       <h1>Can be accessed by any user.</h1>
